162514a492448522fa9eb143a72b6eea8e94a591
kazuma1205 <137795407+kazuma1205@users.noreply.github.com>
2023-07-28 13:46:27 -0500

    xd

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { View, FlatList, Text } from 'react-native';
-import calendario from '../../utilidades/calendario';
 const HomeScreen = () => {
   return (
     <View style={{flex: 1,
