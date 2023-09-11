53f79ceada9375925237eab3e2ba3ffa71d34b97
kazuma1205 <137795407+kazuma1205@users.noreply.github.com>
2023-09-11 15:18:48 -0500

    limpieza de bugs
    no servia la prueba en android pero ahora si, que grande que soy y le puse un boton que abre y cierra el modal

--- navigation/screens/HomeScreen.js
+++ navigation/screens/HomeScreen.js
@@ -1,27 +1,41 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, Platform } from 'react-native';
+import { View, Text, StyleSheet, Platform, TouchableOpacity } from 'react-native';
 import { Calendar } from 'react-native-calendars';
 import MyDatePicker from '../../utilidades/prueba';
 
 function MiCalendario() {
   const [fechaSeleccionada, setFechaSeleccionada] = useState(null);
+  const [calendarVisible, setCalendarVisible] = useState(false);
 
   const handleDateSelect = (date) => {
     setFechaSeleccionada(date.dateString);
   };
 
+  const toggleCalendar = () => {
+    setCalendarVisible(!calendarVisible);
+  };
+
   const renderCalendar = () => {
-    if (Platform.OS === 'ios' || Platform.OS === 'android') {
+    if (Platform.OS === 'ios') {
       // Utiliza el componente DatePicker para iOS y Android
       return (
-       <MyDatePicker/>
+        <MyDatePicker />
       );
     } else {
       // Utiliza el componente MyDatePicker para otras plataformas
-      return  <Calendar
-      onDayPress={handleDateSelect}
-      // Configura otras propiedades del calendario
-    />;
+      return (
+        <View>
+          <TouchableOpacity onPress={toggleCalendar}>
+            <Text>Mostrar Calendario</Text>
+          </TouchableOpacity>
+          {calendarVisible && (
+            <Calendar
+              onDayPress={handleDateSelect}
+              // Configura otras propiedades del calendario
+            />
+          )}
+        </View>
+      );
     }
   };
 
