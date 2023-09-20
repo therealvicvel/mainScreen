831ed02f8d6faa455ca40700513b5bebab466831
kazuma1205 <137795407+kazuma1205@users.noreply.github.com>
2023-09-19 23:17:37 -0500

    prueba inventario sin estilos

--- navigation/screens/guia.js
+++ navigation/screens/guia.js
@@ -1,15 +1,11 @@
-//Importaciones necesarias
 import React from "react";
-import { View, Text, FlatList, Image, Button, Modal, TextInput, ImageBackground,  } from "react-native";
+import { View, Text, FlatList, TouchableOpacity, Modal, TextInput } from "react-native";
 import { useState, useEffect } from "react";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { Picker } from '@react-native-picker/picker';
 import FiltrarBuscar from "../../utilidades/filtrarBuscarProd";
 
-//Creación de lista visual de inventario (declaración de variables y sus datos)
 const ListInventario = () => {
   const [data, setData] = useState([]);
-  const [productoId, setProductoId] = useState('');
   const [cantidad, setCantidad] = useState("");
   const [valorVenta, setValorVenta] = useState("");
   const [valorCompra, setValorCompra] = useState("");
@@ -17,8 +13,6 @@ const ListInventario = () => {
   const [filteredData, setFilteredData] = useState([]);
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [selectedProduct, setSelectedProduct] = useState(null);
-
-
 
   const handleCantidadChange = (text) => {
     setCantidad(text);
@@ -40,8 +34,8 @@ const ListInventario = () => {
         valorVenta: valorVenta,
         valorCompra: valorCompra,
       };
-
-      console.log(productoActualizado);
+  
+      console.log('Producto actualizado:', productoActualizado);
       fetch(`https://viramsoftapi.onrender.com/edit_product/${selectedProduct.idProducto}`, {
         method: "PUT",
         headers: {
@@ -59,7 +53,7 @@ const ListInventario = () => {
             //Actualizar la lista de productos después de guardar los cambios
             setData((prevData) => {
               const newData = prevData.map((item) =>
-                item.idProducto === selectedProducto.idProducto ? productoActualizado : item
+                item.idProducto === selectedProduct.idProducto ? productoActualizado : item
               );
               setIsModalVisible(false);
               return newData;
@@ -75,6 +69,9 @@ const ListInventario = () => {
   
   const handleAddPress = (product) => {
     setSelectedProduct(product);
+    setCantidad(product.cantidad);
+    setValorVenta(product.valorVenta);
+    setValorCompra(product.valorCompra);
     setIsModalVisible(true);
   };
 
@@ -112,101 +109,94 @@ const ListInventario = () => {
       .then((response) => response.json())
       .then((data) => {
         setData(data.productos);
-        console.log(data)
+        console.log(data);
       })
       .catch((error) => {
-        console.error("Error fetching data: ", error)
+        console.error("Error fetching data: ", error);
       });
   }, []);
 
-  //Creación de campos para mostrar en ellos los datos anteriormente creados
   return (
     <View style={{ flex: 1 }}>
-      <FiltrarBuscar Data={filteredData}/>    
-      <View style={{ borderWidth: 1, borderColor: 'black', padding: 10 }}>
-         <Picker
-          selectedValue={selectedCategory}
-          onValueChange={(itemValue) => setSelectedCategory(itemValue)}
-          >
-          <Picker.Item label="Todos" value={""} />
-          <Picker.Item label="Líquidos" value="Líquidos" />
-          <Picker.Item label="Sólidos" value="Sólidos" />
-          <Picker.Item label="Polvos" value="Polvos" />
-          <Picker.Item label="Otro" value="Otro" />
-        </Picker>
-      </View>
-     
-  <FlatList
-    data={filteredData}
-    keyExtractor={(item) => item.idProducto.toString()}
-    numColumns={2}
-    renderItem={({ item }) => (
-      <View style={{ flex: 1 }}>
-        <Text>Nombre: {item.nombre}</Text>
-        <Text>Marca: {item.marca}</Text>
-        <Text>Precio: {item.valorVenta}</Text>
-        <Text>Stock: {item.cantidad}</Text>
-        <TouchableOpacity onPress={() => handleAddPress(item)}>
-          <Text>Actualizar</Text>
-        </TouchableOpacity>
-      </View>
-    )}
-  />
-   <Modal visible={isModalVisible} animationType="slide" transparent={true}>
-    <View>
-      <View style= {{ padding: 16,
-    backgroundColor: '#004187',
-    borderRadius: 20,}}>
-        {selectedProduct && (
-          <>
-            <Text>Actualizar campos de un producto</Text>
-            <Text>ID: {selectedProduct.idProducto}</Text>
-            <Text>Nombre: {selectedProduct.nombre}</Text>
-            <Text>Marca: {selectedProduct.marca}</Text>
-            <Text>Cantidad: {selectedProduct.cantidad}</Text>
-            <Text>Valor compra: {selectedProduct.valorCompra}</Text>
-            <Text>Valor venta: {selectedProduct.valorVenta}</Text>
-            <Text>Unidad medida: {selectedProduct.unidadMedida}</Text>
-            <Text>Categoría: {selectedProduct.categoria}</Text>
-            <Text>Agregar Cambios</Text>
-            <TextInput
-              placeholder="Cantidad"
-              onChangeText={handleCantidadChange}
-              value={cantidad}
-            />
-            <TextInput
-              placeholder="Valor venta"
-              onChangeText={handleValorVentaChange}
-              value={valorVenta}
-            />
-            <TextInput
-              placeholder="Valor compra"
-              onChangeText={handleValorCompraChange}
-              value={valorCompra}
-            />
-          </>
+      <Picker
+        selectedValue={selectedCategory}
+        onValueChange={(itemValue) => setSelectedCategory(itemValue)}
+      >
+        <Picker.Item label="Todos" value={""} />
+        <Picker.Item label="Líquidos" value="Líquidos" />
+        <Picker.Item label="Sólidos" value="Sólidos" />
+        <Picker.Item label="Polvos" value="Polvos" />
+        <Picker.Item label="Otro" value="Otro" />
+      </Picker>
+      <FiltrarBuscar Data={filteredData} onItemSelected={handleAddPress} />
+      <FlatList
+        data={filteredData}
+        keyExtractor={(item) => item.idProducto.toString()}
+        numColumns={2}
+        renderItem={({ item }) => (
+          <View style={{ flex: 1, padding: 16, backgroundColor: '#f0f0f0', margin: 8, borderRadius: 8 }}>
+            <Text>Nombre: {item.nombre}</Text>
+            <Text>Marca: {item.marca}</Text>
+            <Text>Precio: {item.valorVenta}</Text>
+            <Text>Stock: {item.cantidad}</Text>
+            <TouchableOpacity onPress={() => handleAddPress(item)}>
+              <Text>Actualizar</Text>
+            </TouchableOpacity>
+          </View>
         )}
-        <TouchableOpacity onPress={handleCloseModal}>
-          <Text>Cerrar</Text>
-        </TouchableOpacity>
-          <TouchableOpacity onPress={handleGuardarCambios}>
-            <Text>Guardar</Text>
-           </TouchableOpacity>
-         </View>
-         </View>
+      />
+      <Modal visible={isModalVisible} animationType="slide" transparent={true}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ padding: 16, backgroundColor: '#004187', borderRadius: 20, width: '80%' }}>
+            {selectedProduct && (
+              <>
+                <Text style={{ color: 'white' }}>Actualizar campos de un producto</Text>
+                <Text style={{ color: 'white' }}>ID: {selectedProduct.idProducto}</Text>
+                <Text style={{ color: 'white' }}>Nombre: {selectedProduct.nombre}</Text>
+                <Text style={{ color: 'white' }}>Marca: {selectedProduct.marca}</Text>
+                <Text style={{ color: 'white' }}>Cantidad: {selectedProduct.cantidad}</Text>
+                <Text style={{ color: 'white' }}>Valor compra: {selectedProduct.valorCompra}</Text>
+                <Text style={{ color: 'white' }}>Valor venta: {selectedProduct.valorVenta}</Text>
+                <Text style={{ color: 'white' }}>Unidad medida: {selectedProduct.unidadMedida}</Text>
+                <Text style={{ color: 'white' }}>Categoría: {selectedProduct.categoria}</Text>
+                <Text style={{ color: 'white' }}>Agregar Cambios</Text>
+                <TextInput
+                  placeholder="Cantidad"
+                  onChangeText={handleCantidadChange}
+                  value={cantidad}
+                  style={{ backgroundColor: 'white', padding: 8, marginBottom: 8 }}
+                />
+                <TextInput
+                  placeholder="Valor venta"
+                  onChangeText={handleValorVentaChange}
+                  value={valorVenta}
+                  style={{ backgroundColor: 'white', padding: 8, marginBottom: 8 }}
+                />
+                <TextInput
+                  placeholder="Valor compra"
+                  onChangeText={handleValorCompraChange}
+                  value={valorCompra}
+                  style={{ backgroundColor: 'white', padding: 8, marginBottom: 16 }}
+                />
+                <TouchableOpacity onPress={handleCloseModal}>
+                  <Text style={{ color: 'white' }}>Cerrar</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={handleGuardarCambios}>
+                  <Text style={{ color: 'white' }}>Guardar</Text>
+                </TouchableOpacity>
+              </>
+            )}
+          </View>
+        </View>
       </Modal>
     </View>
-    
   );
 };
 
 export default function InventarioScreen({ navigation }) {
   return (
-    <View>
-      <View>
-        <ListInventario />
-      </View>
+    <View style={{ flex: 1, padding: 16 }}>
+      <ListInventario />
     </View>
   );
 }
-
